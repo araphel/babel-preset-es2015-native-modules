d750865aa0500aa38b68065df36f3d712f484b0d
Avindra Goolcharan <Avindra.Goolcharan@adp.com>
2016-04-20 16:23:18 -0400

    Base off of babel-preset-es2015 and just remove the commonjs plugin

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
+var pluginList = require("babel-preset-es2015").plugins;
+
+// Assumption:
+// CommonJS plugin remains second to last in the list.
+// https://github.com/babel/babel/blob/master/packages/babel-preset-es2015/index.js#L22
+
+pluginList.splice(pluginList.length - 2, 1);
+
 module.exports = {
-	plugins: [
-		require("babel-plugin-transform-es2015-template-literals"),
-		require("babel-plugin-transform-es2015-literals"),
-		require("babel-plugin-transform-es2015-function-name"),
-		require("babel-plugin-transform-es2015-arrow-functions"),
-		require("babel-plugin-transform-es2015-block-scoped-functions"),
-		require("babel-plugin-transform-es2015-classes"),
-		require("babel-plugin-transform-es2015-object-super"),
-		require("babel-plugin-transform-es2015-shorthand-properties"),
-		require("babel-plugin-transform-es2015-duplicate-keys"),
-		require("babel-plugin-transform-es2015-computed-properties"),
-		require("babel-plugin-transform-es2015-for-of"),
-		require("babel-plugin-transform-es2015-sticky-regex"),
-		require("babel-plugin-transform-es2015-unicode-regex"),
-		require("babel-plugin-check-es2015-constants"),
-		require("babel-plugin-transform-es2015-spread"),
-		require("babel-plugin-transform-es2015-parameters"),
-		require("babel-plugin-transform-es2015-destructuring"),
-		require("babel-plugin-transform-es2015-block-scoping"),
-		require("babel-plugin-transform-es2015-typeof-symbol"),
-		[require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }],
-	]
+	plugins: pluginList
 };
